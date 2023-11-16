61b5e94ec9c4d5ce400c7f4dd5b138f106a052cf
German Renato Cobian <germancobian@hotmail.com>
2023-11-16 10:58:51 -0800

    Substitute redirect for input on Login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,28 +8,31 @@ const Login = () => {
 
   return (
     <main className="background d-flex flex-column align-items-center">
-        <div className="d-flex flex-row">
-          <h3 className="my-5">LOGIN TO YOUR ACCOUNT</h3>
+      <div className="d-flex flex-row">
+        <h3 className="my-5">LOGIN TO YOUR ACCOUNT</h3>
+      </div>
+      <form class="border boder-dark rounded mb-2 mx-5 w-50">
+        <img className="logo ps-5 pt-3" src={Logo} alt="Journeyman logo"  />
+        <div class="form-group d-flex flex-column align-items-start my-3 mx-5">
+          <p><strong>Email address</strong></p>
+          <input type="email" class="form-control px-5" id="email1" aria-describedby="emailHelp" placeholder="Enter email" />
+          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
-        <form class="border boder-dark rounded mb-5 mx-5 w-50">
-          <img className="logo ps-5 pt-3" src={Logo} alt="Journeyman logo"  />
-          <div class="form-group d-flex flex-column align-items-start my-3 mx-5">
-            <p><strong>Email address</strong></p>
-            <input type="email" class="form-control px-5" id="email1" aria-describedby="emailHelp" placeholder="Enter email" />
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-          </div>
-          <div class="form-group d-flex flex-column align-items-start my-3 mx-5">
-            <p><strong>Password</strong></p>
-            <input type="password" class="form-control px-5" id="password1" placeholder="Password" />
-          </div>
-          <div class="d-flex flex-row justify-content-start my-2 ms-5">
-            <input type="checkbox" class="me-3" id="exampleCheck1" />
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-          </div>
-          <div class="d-flex flex-row justify-content-end">
-          <NavLink to="/chat/1" activeClassName="active-link" className="btn btn-outline-primary mt-2 mb-5 me-5 py-0" ><span><small>Login</small></span></NavLink>
-          </div>
-        </form>
+        <div class="form-group d-flex flex-column align-items-start my-3 mx-5">
+          <p><strong>Password</strong></p>
+          <input type="password" class="form-control px-5" id="password1" placeholder="Password" />
+        </div>
+        <div class="d-flex flex-row justify-content-start my-2 ms-5">
+          <input type="checkbox" class="me-3" id="exampleCheck1" />
+          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <div class="d-flex flex-row justify-content-end">
+          <button type="submit" class="btn btn-outline-primary mt-2 mb-5 me-5 py-0">Submit</button>
+        </div>
+      </form>
+      <div class="d-flex flex-row justify-content-start">
+        <NavLink to="/chat/1" activeClassName="active-link" className="mb-5 me-5 py-0" ><span><small>Go to Chat</small></span></NavLink>
+      </div>
     </main>
   );
 };
